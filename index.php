c627aab7145c7416e4fb60265778eb10375e773a
Scott <scottsaunders@mac.com>
2022-04-14 19:52:13 -0400

    added sample copy to all sections.

--- index.php
+++ index.php
@@ -114,6 +114,40 @@
 				<polygon  points="22,17.4 4.6,3.3 5.9,1.7 22,14.8 38.1,1.7 39.4,3.3 "/>
 				</svg>
 			</div>
+			
+			<div class="main-container">
+				
+				<div class="flex-container">
+				<div class="flex-column">
+					<p><img src="https://s3.amazonaws.com/imglibs/fiat.jpeg"></p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>
+				
+				<div class="flex-column">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper augue nunc, vitae gravida tellus posuere ultricies. In suscipit magna id quam malesuada convallis. Maecenas et augue maximus, facilisis leo sed, ultrices tortor. Praesent varius eleifend porta. Nulla eget lorem id lectus placerat luctus. Curabitur ultricies turpis velit, imperdiet ultricies augue ullamcorper varius. Vivamus malesuada rutrum fermentum. Pellentesque tincidunt sapien cursus ante efficitur, vitae tincidunt ante sodales. Duis odio est, congue at nibh et, commodo efficitur massa. Morbi porttitor elit ac sem rhoncus ornare. Curabitur commodo aliquam auctor.</p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>	
+				</div>			
+			<div class="flex-container flex-reverse">
+				<div class="flex-column">
+					<p><img src="https://s3.amazonaws.com/imglibs/fiat.jpeg"></p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>
+				
+				<div class="flex-column">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper augue nunc, vitae gravida tellus posuere ultricies. In suscipit magna id quam malesuada convallis. Maecenas et augue maximus, facilisis leo sed, ultrices tortor. Praesent varius eleifend porta. Nulla eget lorem id lectus placerat luctus. Curabitur ultricies turpis velit, imperdiet ultricies augue ullamcorper varius. Vivamus malesuada rutrum fermentum. Pellentesque tincidunt sapien cursus ante efficitur, vitae tincidunt ante sodales. Duis odio est, congue at nibh et, commodo efficitur massa. Morbi porttitor elit ac sem rhoncus ornare. Curabitur commodo aliquam auctor.</p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>	
+				</div>			
+			
+				
+			</div>
+
+			
 		</div>
 		
 	</div>
@@ -146,6 +180,39 @@
 				</svg>
 			</div>
 			
+			<div class="main-container">
+				
+				<div class="flex-container">
+				<div class="flex-column">
+					<p><img src="https://s3.amazonaws.com/imglibs/fiat.jpeg"></p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>
+				
+				<div class="flex-column">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper augue nunc, vitae gravida tellus posuere ultricies. In suscipit magna id quam malesuada convallis. Maecenas et augue maximus, facilisis leo sed, ultrices tortor. Praesent varius eleifend porta. Nulla eget lorem id lectus placerat luctus. Curabitur ultricies turpis velit, imperdiet ultricies augue ullamcorper varius. Vivamus malesuada rutrum fermentum. Pellentesque tincidunt sapien cursus ante efficitur, vitae tincidunt ante sodales. Duis odio est, congue at nibh et, commodo efficitur massa. Morbi porttitor elit ac sem rhoncus ornare. Curabitur commodo aliquam auctor.</p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>	
+				</div>			
+			<div class="flex-container flex-reverse">
+				<div class="flex-column">
+					<p><img src="https://s3.amazonaws.com/imglibs/fiat.jpeg"></p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>
+				
+				<div class="flex-column">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper augue nunc, vitae gravida tellus posuere ultricies. In suscipit magna id quam malesuada convallis. Maecenas et augue maximus, facilisis leo sed, ultrices tortor. Praesent varius eleifend porta. Nulla eget lorem id lectus placerat luctus. Curabitur ultricies turpis velit, imperdiet ultricies augue ullamcorper varius. Vivamus malesuada rutrum fermentum. Pellentesque tincidunt sapien cursus ante efficitur, vitae tincidunt ante sodales. Duis odio est, congue at nibh et, commodo efficitur massa. Morbi porttitor elit ac sem rhoncus ornare. Curabitur commodo aliquam auctor.</p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>	
+				</div>			
+			
+				
+			</div>
+
+			
 		</div>
 		
 	</div>
@@ -175,6 +242,39 @@
 				<polygon  points="22,17.4 4.6,3.3 5.9,1.7 22,14.8 38.1,1.7 39.4,3.3 "/>
 				</svg>
 			</div>
+			
+			<div class="main-container">
+				
+				<div class="flex-container">
+				<div class="flex-column">
+					<p><img src="https://s3.amazonaws.com/imglibs/fiat.jpeg"></p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>
+				
+				<div class="flex-column">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper augue nunc, vitae gravida tellus posuere ultricies. In suscipit magna id quam malesuada convallis. Maecenas et augue maximus, facilisis leo sed, ultrices tortor. Praesent varius eleifend porta. Nulla eget lorem id lectus placerat luctus. Curabitur ultricies turpis velit, imperdiet ultricies augue ullamcorper varius. Vivamus malesuada rutrum fermentum. Pellentesque tincidunt sapien cursus ante efficitur, vitae tincidunt ante sodales. Duis odio est, congue at nibh et, commodo efficitur massa. Morbi porttitor elit ac sem rhoncus ornare. Curabitur commodo aliquam auctor.</p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>	
+				</div>			
+			<div class="flex-container flex-reverse">
+				<div class="flex-column">
+					<p><img src="https://s3.amazonaws.com/imglibs/fiat.jpeg"></p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>
+				
+				<div class="flex-column">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper augue nunc, vitae gravida tellus posuere ultricies. In suscipit magna id quam malesuada convallis. Maecenas et augue maximus, facilisis leo sed, ultrices tortor. Praesent varius eleifend porta. Nulla eget lorem id lectus placerat luctus. Curabitur ultricies turpis velit, imperdiet ultricies augue ullamcorper varius. Vivamus malesuada rutrum fermentum. Pellentesque tincidunt sapien cursus ante efficitur, vitae tincidunt ante sodales. Duis odio est, congue at nibh et, commodo efficitur massa. Morbi porttitor elit ac sem rhoncus ornare. Curabitur commodo aliquam auctor.</p>
+
+<p>Donec erat leo, maximus vitae sodales vitae, egestas et orci. Sed scelerisque aliquam mi, et imperdiet quam scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sodales, nisl eget dictum consectetur, augue diam sodales leo, at mollis augue ipsum et tortor. Mauris scelerisque augue id mi congue, eu vestibulum tortor vestibulum. Maecenas lobortis feugiat velit, et dignissim leo suscipit ac. Nam iaculis vitae arcu quis facilisis. Donec at urna odio. Maecenas id orci sit amet ipsum tempor lacinia. Vestibulum semper luctus nibh ac rutrum. Etiam convallis dolor et justo congue, eu consectetur lectus ultrices. Nullam eu massa hendrerit, tristique nunc et, vestibulum dolor.</p>
+				</div>	
+				</div>			
+			
+				
+			</div>
+
 			
 		</div>
 		
